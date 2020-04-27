910ec645635a54c7566e1c33cc643c57b623d4e1
Michael Peter Christen <mc@yacy.net>
2020-04-27 19:08:33 +0200

    added test file for local deepspeech

---
+++ bin/test_local.sh
@@ -0,0 +1,7 @@
+deepspeech --model ~/deepspeech/deepspeech-0.7.0-models.pbmm --scorer ~/deepspeech/deepspeech-0.7.0-models.scorer --audio ../wav/pangram_en_alex.wav
+deepspeech --model ~/deepspeech/deepspeech-0.7.0-models.pbmm --scorer ~/deepspeech/deepspeech-0.7.0-models.scorer --audio ../wav/pangram_en_daniel.wav
+deepspeech --model ~/deepspeech/deepspeech-0.7.0-models.pbmm --scorer ~/deepspeech/deepspeech-0.7.0-models.scorer --audio ../wav/pangram_en_fiona.wav
+deepspeech --model ~/deepspeech/deepspeech-0.7.0-models.pbmm --scorer ~/deepspeech/deepspeech-0.7.0-models.scorer --audio ../wav/pangram_en_karen.wav
+deepspeech --model ~/deepspeech/deepspeech-0.7.0-models.pbmm --scorer ~/deepspeech/deepspeech-0.7.0-models.scorer --audio ../wav/pangram_en_moira.wav
+deepspeech --model ~/deepspeech/deepspeech-0.7.0-models.pbmm --scorer ~/deepspeech/deepspeech-0.7.0-models.scorer --audio ../wav/pangram_en_samantha.wav
+deepspeech --model ~/deepspeech/deepspeech-0.7.0-models.pbmm --scorer ~/deepspeech/deepspeech-0.7.0-models.scorer --audio ../wav/pangram_en_tessa.wav
